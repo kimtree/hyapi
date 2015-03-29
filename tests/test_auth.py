e4a254b6099e46de3476c1f934379b7c884ad6d4
Namwoo Kim <iam@kimtree.net>
2015-03-29 21:09:00 +0900

    Add a unittest and travis configuration

---
+++ tests/test_auth.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from hyauth.auth import HYAuthHandler
+
+
+class HYAuthHandlerTests(unittest.TestCase):
+
+    def test_get_code_url(self):
+        auth = HYAuthHandler('a', 'b', 'c', 'd')
+        self.assertEqual('https://api.hanyang.ac.kr/oauth/authorize?scope=c&redirect_uri=d&response_type=code&client_id=a', auth.get_code_url())  # noqa
