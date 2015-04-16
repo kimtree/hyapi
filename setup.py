fc80a02d30ced350edcf03c1aeb499fffec374ab
kimtree <nw.kim@ridi.com>
2015-04-16 18:44:11 +0900

    Change version info

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='hyapi',
-      version='0.1.0',
+      version='0.0.1',
       description='Python SDK for Hanyang Univ API',
       url='http://github.com/kimtree/hyapi',
       author='Namwoo Kim',
